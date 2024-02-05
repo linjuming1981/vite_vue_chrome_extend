bd7a3d7b51ee89e7990f4d70770f92b9787c00af
linjuming1981 <mingfish@qq.com>
2024-02-05 15:17:29 +0800

    aaaaa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import {crx} from '@crxjs/vite-plugin';
-import manifest from 'manifest_v3.json'
+import manifest from './manifest_v3.json'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
