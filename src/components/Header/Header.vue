c4a4521229b3f55fc3b435b9e97fb61a55aa36f5
linjuming1981 <mingfish@qq.com>
2024-02-06 19:21:12 +0800

    aa

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-checkbox-group v-model="action">
+      <el-checkbox-button label="header">添加header</el-checkbox-button>
+      <el-checkbox-button label="other">其他</el-checkbox-button>
+    </el-checkbox-group>
+  </div>
+</template>
+<script>
+
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+      action: 'header'
+    };
+  },
+  computed: {},
+  watch: {
+    action(val){
+      this.$emit('setAction', val)
+    }
+  },
+  created () {},
+  mounted () {},
+  methods: {},
+};
+</script>
+<style lang="less" scoped>
+</style>
