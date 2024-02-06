4192d915c3451a5a16dd296e5da1b1547ef353ce
linjuming1981 <mingfish@qq.com>
2024-02-06 19:29:28 +0800

    aaa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import {crx} from '@crxjs/vite-plugin';
 import manifest from './manifest_v3.json'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path';
+import Components from 'unplugin-vue-components/vite';
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +19,8 @@ export default defineConfig({
   plugins: [
     vue(),
     crx({manifest}),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
   ],
 })
