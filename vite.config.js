34f59dfb20b5031aeeced8babb7e64f0408c289f
linjuming1981 <mingfish@qq.com>
2024-02-06 16:38:46 +0800

    ddd

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    }
+  },
   plugins: [
     vue(),
     crx({manifest}),
