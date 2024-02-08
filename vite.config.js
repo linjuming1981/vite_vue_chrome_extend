2346bd98ce24ab62424da408d95ca341f9d3f688
linjuming1981 <mingfish@qq.com>
2024-02-08 14:48:04 +0800

    ddddddd

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
 export default defineConfig({
   build: {
     minify: false,
+    rollupOptions: {
+      input: '/index.html',
+    }
   },
   resolve: {
     alias: {
